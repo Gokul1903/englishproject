09da43b1689f5c4a0d1f7de2a869e8e86cea6c2a
gokul1912 <rajgokul436@gmail.com>
2024-02-22 07:11:15 +0530

    added doyouknow to chap1

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function showoverlay(){
+    var overlay=document.getElementById("overlay");
+    var overlay_content=document.getElementById("overlay-content");
+    overlay.style.display ="block";
+    overlay_content.style.display="block";
+    
+}
+function clear_btn(){
+    var overlay=document.getElementById("overlay");
+    var overlay_content=document.getElementById("overlay-content");
+    overlay.style.display ="none";
+    overlay_content.style.display="none";
+}
